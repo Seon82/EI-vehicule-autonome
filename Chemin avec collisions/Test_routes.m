62c2cad845d62b1c720179365546c5cd526011f8
Maxime Guerini <maximeguerini@student-cs.fr>
2020-11-11 18:56:01 +0100

    temp changes

--- Chemin avec collisions/Test_routes.m
+++ Chemin avec collisions/Test_routes.m
@@ -5,11 +5,20 @@ close all;
 Ts = 0.1;
 scenario.SampleTime = Ts;
 
+%Initial node posisitons for cars, one integer = one car
+CAR_NODE_POSITION = [0];
+
+%Initial speeds, one integer = one speed
+CAR_SPEED = [2];
+
+%Create cars
+%cars = addCars(scenario, CAR_NODE_POSITION);
+
 car = vehicle(scenario, 'ClassID', 1, 'Position', [0 0 0]);
-vitesse = 2;
 
 waypoints = [1 2 3 4 5 12 11 10 18 17 16 15 8 7 6 13 23 24 21 22 25 26 20 19 11];
 i = 1;
+vitesse =2;
 
 plot(scenario);
 while advance(scenario)
