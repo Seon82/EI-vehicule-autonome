7270d45d3591b474eb74fcd241cd8119b9934677
Dylan Sechet <dylan.sechet82@gmail.com>
2020-11-12 13:25:17 +0100

    Chanegd flags

--- Chemin avec collisions/Fonctions utilisées par le Script/compute_new_path.m
+++ Chemin avec collisions/Fonctions utilisées par le Script/compute_new_path.m
@@ -1,4 +1,4 @@
 function [new_index, new_waypoints, updated_graph]= compute_new_path(wp_index, waypoints, graphe)
-updated_graph = rmedge(graphe, waypoints(wp_index-1), waypoints(wp_index));
-new_waypoints = [waypoints(1:max(1, wp_index-2)), shortestpath(updated_graph, waypoints(wp_index-1), waypoints(end))];
+    updated_graph = rmedge(graphe, waypoints(wp_index-1), waypoints(wp_index));
+    new_waypoints = [waypoints(1:max(1, wp_index-2)), shortestpath(updated_graph, waypoints(wp_index-1), waypoints(end))];
 new_index = wp_index - 1;
